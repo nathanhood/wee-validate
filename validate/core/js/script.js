80f355ea00c9b54b5580aa6334281614851dc598
Ryan Payne <ryan@caddis.co>
2016-08-10 10:39:54 -0500

    Break up methods

--- validate/core/js/script.js
+++ validate/core/js/script.js
@@ -1,90 +1,35 @@
 Wee.fn.make('validate', {
 	_construct: function() {
 		this.namespace = 'formValidator';
+		this.selector = 'ref:formField';
+		this.errorClass = '-error';
+		this.errorSelector = 'ref:formError';
 	},
 
 	/**
-	 * Init validation
+	 * Init validation via form submission
 	 *
 	 * @param {object} options
 	 * @param {string} [options.formSelector=ref:form] - Selector for the form.
-	 * @param {string} [options.inputSelector=ref:formField] - Selector for the form inputs.
-	 * @param {string} [options.errorClass=-error] - Class applied to the input on error.
-	 * @param {string} [options.errorElementSelector=ref:formError] - Selector for the error element.
-	 * @param {string} [options.view=<span class="form-error" data-ref="formError">{{ label ? label : "This field" }} is required.</span>] - The view template or reference to a template to output the error.
-	 * @param {boolean} [options.scrollTop=true] - Scroll to the top of the form if there is an error.
 	 * @param {boolean} [options.ajaxRequest=false] - Prevents form submission on successful validation.
 	 * @param {(Array|function|string)} [options.onValid] - Callback when form validates. Passes form object as first param.
 	 * @param {(Array|function|string)} [options.onInvalid] - Callback when form is invalid.
 	 */
 	init: function(options) {
-		var settings = $.extend({
+		var scope = this,
+			settings = $.extend({
 				formSelector: 'ref:form',
-				inputSelector: 'ref:formField',
-				errorClass: '-error',
-				errorElementSelector: 'ref:formError',
-				view: '<span class="form-error" data-ref="formError">{{ label ? label : "This field" }} is required.</span>',
-				scrollTop: true,
 				ajaxRequest: false
 			}, options),
-			errorClass = settings.errorClass,
-			errorElementSelector = settings.errorElementSelector,
 			validCallback = settings.onValid,
 			invalidCallback = settings.onInvalid;
 
-		$(settings.inputSelector).on('focus.' + this.namespace, function(e, el) {
-			$(el).removeClass(errorClass)
-				.siblings()
-				.removeClass(errorClass)
-				.siblings(errorElementSelector)
-				.remove();
-		});
+		scope.bindFields(settings);
 
 		$(settings.formSelector).on('submit.' + this.namespace, function(e, el) {
-			var $el = $(el),
-				$fields = $el.find('[data-required]');
+			scope.clear(settings);
 
-			// Clear out all errors
-			$fields.removeClass(errorClass)
-				.siblings('label')
-				.removeClass(errorClass);
-			$(errorElementSelector).remove();
-
-			if (
-				$fields.map(function(el) {
-					return $(el).val() === '';
-				}).length > 0
-			) {
-				$fields.each(function(el) {
-					var $el = $(el);
-
-					// If an item's value is empty, add the error after the input
-					if (! $el.val()) {
-						var view = settings.view,
-							model = {
-								label: $el.attr('data-label') ? $el.data('label') : ''
-							};
-
-						$el.addClass(errorClass)
-							.siblings('label')
-							.addClass(errorClass);
-
-						// Check if the next item is a label
-						if ($el.next('label').length) {
-							$el.after($.view.render(view, model))
-						} else {
-							$el.parent()
-								.append($.view.render(view, model));
-						}
-					}
-				});
-
-				if (settings.scrollTop) {
-					$(Wee._body).tween({
-						scrollTop: $el.offset().top
-					});
-				}
-
+			if (! scope.isValid(settings)) {
 				if (invalidCallback) {
 					invalidCallback();
 				}
@@ -92,13 +37,127 @@ Wee.fn.make('validate', {
 				e.preventDefault();
 			} else {
 				if (validCallback) {
-					validCallback($el);
+					validCallback($(el));
 				}
 			}
 
 			if (settings.ajaxRequest) {
 				e.preventDefault();
 			}
+		});
+	},
+
+	/**
+	 * Check for validation errors
+	 *
+	 * @param {object} options
+	 * @param {string} [options.selector=ref:formField] - Selector for the form field.
+	 * @param {string} [options.errorClass=-error] - Class applied to the field on error.
+	 * @param {string} [options.errorSelector=ref:formError] - Selector for the error element.
+	 * @param {string} [options.view=<span class="form-error" data-ref="formError">{{ label ? label : "This field" }} is required.</span>] - The view template or reference to a template to output the error.
+	 * @param {(boolean|number|selector)} [options.scrollTop=0] - Scroll point if there are errors.
+	 * @return {boolean}
+	 */
+	isValid: function(options) {
+		var settings = $.extend({
+				selector: this.selector,
+				errorClass: this.errorClass,
+				errorSelector: this.errorSelector,
+				view: '<span class="form-error" data-ref="formError">{{ label ? label : "This field" }} is required.</span>',
+				scrollTop: 0
+			}, options),
+			errorClass = settings.errorClass,
+			$fields = $(settings.selector).filter('[data-required]'),
+			scrollTop = settings.scrollTop;
+
+		// Clear out existing errors
+		this.clear(settings);
+
+		if (
+			$fields.map(function(el) {
+				return $(el).val() === '';
+			}).length > 0
+		) {
+			$fields.each(function(el) {
+				var $el = $(el);
+
+				// If an item's value is empty, add the error after the input
+				if (! $el.val()) {
+					var view = settings.view,
+						model = {
+							label: $el.attr('data-label') ? $el.data('label') : ''
+						};
+
+					$el.addClass(errorClass)
+						.siblings('label')
+						.addClass(errorClass);
+
+					// Check if the next item is a label
+					if ($el.next('label').length) {
+						$el.after($.view.render(view, model));
+					} else {
+						$el.parent()
+							.append($.view.render(view, model));
+					}
+				}
+			});
+
+			if (scrollTop !== false) {
+				$(Wee._body).tween({
+					scrollTop: typeof scrollTop == 'number' ? scrollTop : $(scrollTop).offset().top
+				});
+			}
+
+			return false;
+		}
+
+		return true;
+	},
+
+	/**
+	 * Clear out form errors
+	 *
+	 * @param {object} options
+	 * @param {string} [options.errorClass=-error] - Class applied to the field on error.
+	 * @param {string} [options.errorSelector=ref:formError] - Selector for the error element.
+	 */
+	clear: function(options) {
+		var settings = $.extend({
+				errorClass: this.errorClass,
+				errorSelector: this.errorSelector
+			}, options),
+			errorClass = settings.errorClass,
+			$fields = $(settings.selector).filter('[data-required]');
+
+		$fields.removeClass(errorClass)
+			.siblings('label')
+			.removeClass(errorClass);
+
+		$(settings.errorSelector).remove();
+	},
+
+	/**
+	 * Bind form error clearing for fields
+	 *
+	 * @param {object} options
+	 * @param {string} [options.selector=ref:formField] - Selector for the form field.
+	 * @param {string} [options.errorClass=-error] - Class applied to the field on error.
+	 * @param {string} [options.errorSelector=ref:formError] - Selector for the error element.
+	 */
+	bindFields: function(options) {
+		var settings = $.extend({
+				selector: this.selector,
+				errorClass: this.errorClass,
+				errorSelector: this.errorSelector
+			}, options),
+			errorClass = settings.errorClass;
+
+		$(settings.selector).filter('[data-required]').on('focus.' + this.namespace, function(e, el) {
+			$(el).removeClass(errorClass)
+				.siblings()
+				.removeClass(errorClass)
+				.siblings(settings.errorSelector)
+				.remove();
 		});
 	},
 
